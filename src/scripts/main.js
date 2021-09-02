9e896596998dc491d9d35f539199c84bfd7c648a
Susie Stanley <susiestanley00@gmail.com>
2021-09-02 11:43:58 -0500

    first commit after creating repo

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+import { getUsers } from "./data/dataManager.js";
+import { getPosts } from "./data/dataManager.js";
+
+const allUsers = getUsers()
+.then(apiUsers => {
+    console.log("now we can console the users", apiUsers)
+})
+
+console.log("All users: ", allUsers);
+
+
+
+const allPosts = getPosts();
+console.log("All posts: ", allPosts);
