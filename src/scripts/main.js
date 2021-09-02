05df1cb70f6cc8edfa370b34e453cbfee4c8b4d1
Susie Stanley <susiestanley00@gmail.com>
2021-09-02 12:05:15 -0500

    updated json and js files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,11 @@ const allUsers = getUsers()
 .then(apiUsers => {
     console.log("now we can console the users", apiUsers)
 })
-
 console.log("All users: ", allUsers);
 
 
-
-const allPosts = getPosts();
+const allPosts = getPosts()
+.then(apiPosts => {
+    console.log("now we can console the posts", apiPosts)
+})
 console.log("All posts: ", allPosts);
