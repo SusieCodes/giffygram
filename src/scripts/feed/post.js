24c31af462adbb867a9696788bf1f4ec865e8a76
Susie Stanley <susiestanley00@gmail.com>
2021-09-03 23:42:17 -0500

    updated html, css, and javascript

--- src/scripts/feed/post.js
+++ src/scripts/feed/post.js
@@ -8,9 +8,7 @@ export const Post = (postObject) => {
     const showDate = formatDate(postObject.timestamp);
     return `
       <section class="post">
-        <header>
             <h2 class="post__title"> ${postObject.title} </h2>
-        </header>
         <img class="post__image" src="${postObject.imageURL}" />
 
         <div class="details">
--- src/scripts/nav/footer.js
+++ src/scripts/nav/footer.js
@@ -4,9 +4,21 @@ export const Footer = () => {
                 <img src="./images/pb.png" alt="Giffygram icon" />
             </div>
             <div class="footer__item footer__name">
-                <p>Giffygram</p>
+                <p class="giffy">Giffygram</p>
                 <p>&copy;2021 Susie Stanley</p>
             </div>
-            <div class="footer__item"><p>dropdown goes here</p></div>
+            
+            <div class="dropup footer__right">
+                <button class="dropbtn">View By Year&nbsp;&nbsp;▲</button>
+                <div class="dropup-content">
+                <a href="#">2018</a>
+                <a href="#">2019</a>
+                <a href="#">2020</a>
+                <a href="#">2021</a>
+                <a href="#">2022</a>
+                </div> <!-- closes dropbtn -->
+            </div> <!-- closes dropup -->
+
+            </p></div>
     `
 }
