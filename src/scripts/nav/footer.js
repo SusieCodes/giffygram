ca7e0621b9f91ed8bcf704d9a406efef2881fe87
Susie Stanley <susiestanley00@gmail.com>
2021-09-05 16:35:34 -0500

    updated dropup menu to change text inside button to match year clicked

--- src/scripts/nav/footer.js
+++ src/scripts/nav/footer.js
@@ -15,7 +15,7 @@ export const Footer = () => {
                 </div> <!-- closes col1-->
 
                 <div class="col2">
-                    <div class="dropup-menu">
+                    <div id="yearChoice" class="dropup-menu">
                          
                             <button class="menu-btn">Choose</button>
                             <div class="menu-content">
@@ -26,7 +26,7 @@ export const Footer = () => {
                             <a id="seventeen" class="links" href="#">2017</a>
                             </div> <!-- closes menu-content -->
             
-                    </div> <!-- closes dropup-menu -->
+                    </div> <!-- closes yearChoice dropup-menu -->
                 </div> <!-- closes col2-->
 
                 <div class="col3"><div> =  &nbsp;</div><div id="postCount">0</div>
@@ -34,4 +34,18 @@ export const Footer = () => {
 
             </div> <!-- closes footer-flex -->
     `
+}
+
+export const changeBtn = (year) => {
+	const btnElement = document.querySelector("#yearChoice");
+	btnElement.innerHTML = `
+        <button class="menu-btn">${year}</button>
+        <div class="menu-content">
+        <div id="twentyone" class="links">2021</div>
+        <a id="twenty" class="links" href="#">2020</a>
+        <a id="nineteen" class="links" href="#">2019</a>
+        <a id="eighteen" class="links" href="#">2018</a>
+        <a id="seventeen" class="links" href="#">2017</a>
+        </div> <!-- closes menu-content -->
+        `
 }
