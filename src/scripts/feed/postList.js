4e61d1fe878ec9424618d87c197651b80e506922
Susie Stanley <susiestanley00@gmail.com>
2021-09-05 22:02:34 -0500

    updated css

--- src/scripts/feed/postList.js
+++ src/scripts/feed/postList.js
@@ -1,6 +1,5 @@
 import { Post } from "./post.js";
 
-
 export const postList = (allPosts) => {
 	let postHTML = "";
 		for (const postObject of allPosts) {
@@ -9,7 +8,6 @@ export const postList = (allPosts) => {
 		return postHTML;
 	
 }
-
 
 const findYear = (obj) => {
     const dateStr = new Date(obj);
@@ -62,57 +60,3 @@ export const yearList = (allPosts, year) => {
 	}
 }
 
-// export const seventeenList = (allPosts) => {
-// 	let yearHTML = "";
-// 	for (const postObject of allPosts) {
-// 		let postYear = findYear(postObject.timestamp);
-// 		if (postYear === 2017) {
-// 			yearHTML += Post(postObject)
-// 		} 
-// 	}
-// 	return yearHTML;
-// }
-
-// export const eighteenList = (allPosts) => {
-// 	let yearHTML = "";
-// 		for (const postObject of allPosts) {
-// 			let postYear = findYear(postObject.timestamp);
-// 			if (postYear === 2018) {
-// 				yearHTML += Post(postObject)
-// 		    } 
-// 		}
-// 		return yearHTML;
-// }
-
-// export const nineteenList = (allPosts) => {
-// 	let yearHTML = "";
-// 		for (const postObject of allPosts) {
-// 			let postYear = findYear(postObject.timestamp);
-// 			if (postYear === 2019) {
-// 				yearHTML += Post(postObject)
-// 		    } 
-// 		}
-// 		return yearHTML;
-// }
-
-// export const twentyList = (allPosts) => {
-// 	let yearHTML = "";
-// 		for (const postObject of allPosts) {
-// 			let postYear = findYear(postObject.timestamp);
-// 			if (postYear === 2020) {
-// 				yearHTML += Post(postObject)
-// 		    } 
-// 		}
-// 		return yearHTML;
-// }
-
-// export const twentyOneList = (allPosts) => {
-// 	let yearHTML = "";
-// 		for (const postObject of allPosts) {
-// 			let postYear = findYear(postObject.timestamp);
-// 			if (postYear === 2021) {
-// 				yearHTML += Post(postObject)
-// 		    } 
-// 		}
-// 		return yearHTML;	
-// }
