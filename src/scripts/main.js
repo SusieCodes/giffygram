f13e701e5766934e33308ba0e387d88384fb4216
Susie Stanley <susiestanley00@gmail.com>
2021-09-20 15:18:46 -0500

    got all the bugs out so now edit delete and cancel buttons all work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,4 @@
 import { getUsers, getPosts, usePostCollection, createPost, deletePost, getSinglePost, getLoggedInUser, updatePost } from "./data/dataManager.js";
-
-// import { getJokes } from "./data/dadJoke.js";
 import { navBar } from "./nav/navBar.js";
 import { Footer, changeBtn } from "./nav/footer.js";
 import { postList, yearList } from "./feed/postList.js";
@@ -56,21 +54,6 @@ const startGiffyGram = () => {
 
 startGiffyGram();
 
-
-// start dadjokes section *******************
-
-// const startJokes = () => {
-//     const jokeElement = document.querySelector(".jokes");
-// 	getJokes().then(apiJoke => {
-//     jokeElement.innerHTML = `<h3>${apiJoke.joke}</h3>`;
-//     })
-// }
-
-// var btn = document.getElementById("jokes-btn");
-// btn.addEventListener("click", startJokes);
-
-// end dadjokes section *********************
-
 const handleGiffyClick = (event) => {
     if (event.target.id === "logout") {
         console.log("You clicked on logout")
@@ -99,14 +82,14 @@ const handleGiffyClick = (event) => {
     }
 }
 
+// saves info for the event listeners to access
 const applicationElement = document.querySelector(".giffygram");
-const changePostsElement = document.querySelector(".postList");
 
+//event listener for post message and logout in header and year selection in footer 
 applicationElement.addEventListener("click", handleGiffyClick)
 
-
 //event listener for edit and delete button
-changePostsElement.addEventListener("click", (event) => {
+applicationElement.addEventListener("click", (event) => {
 	if (event.target.id.startsWith("edit")){
 		console.log("post clicked to edit", event.target.id.split("--"))
 		console.log("the id is", event.target.id.split("--")[1])
@@ -116,17 +99,111 @@ changePostsElement.addEventListener("click", (event) => {
         showEdit(response);
         window.scrollTo({ top: 0, behavior: "smooth" })
 	})
+    }
     if (event.target.id.startsWith("delete")) {
         const postId = event.target.id.split("--")[1];
         deletePost(postId)
         .then(response => {
         showPostList();
+        window.scrollTo({ top: 0, behavior: "smooth" })
         })
 		console.log("post clicked to delete", event.target.id.split("--"))
 		console.log("the id is", event.target.id.split("--")[1])
   }
+})
+
+// event listener for new post submit and cancel (new and edit)
+applicationElement.addEventListener("click", event => {
+event.preventDefault();
+if (event.target.id === "new-post__submit") {
+    console.log("you clicked new-post-submit button")
+    const title = document.querySelector("input[name='postTitle']").value
+    const url = document.querySelector("input[name='postURL']").value
+    const description = document.querySelector("textarea[name='postDescription']").value
+    //we have not created a user yet - for now, we will hard code `1`.
+    const postObject = {
+        title: title,
+        imageURL: url,
+        description: description,
+        userId: 1,
+        timestamp: Date.now()
+    }
+createPost(postObject).then(dbResponse => {
+    showPostList();
+    showPostEntry();
+});
+} else if (event.target.id === "new-post__cancel") {
+    console.log("you clicked new-post-cancel button")
+    showPostEntry();
 }
 })
+ 
+// event listener for new post update
+applicationElement.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.id.startsWith("updatePost")) {
+        console.log("you clicked updatePost button")
+      const postId = event.target.id.split("__")[1];
+      //collect all the details into an object
+      const title = document.querySelector("input[name='postTitle']").value
+      const url = document.querySelector("input[name='postURL']").value
+      const description = document.querySelector("textarea[name='postDescription']").value
+      const timestamp = document.querySelector("input[name='postTime']").value
+      
+      const postObject = {
+        title: title,
+        imageURL: url,
+        description: description,
+        userId: getLoggedInUser().id,
+        timestamp: parseInt(timestamp),
+        id: parseInt(postId)
+      }
+      
+      updatePost(postObject)
+        .then(response => {
+          showPostList();
+          showPostEntry();
+        })
+    }
+  })
+
+// import { getJokes } from "./data/dadJoke.js";
+
+  // start dadjokes section *******************
+
+// const startJokes = () => {
+//     const jokeElement = document.querySelector(".jokes");
+// 	getJokes().then(apiJoke => {
+//     jokeElement.innerHTML = `<h3>${apiJoke.joke}</h3>`;
+//     })
+// }
+
+// var btn = document.getElementById("jokes-btn");
+// btn.addEventListener("click", startJokes);
+
+// end dadjokes section *********************
+
+// applicationElement.addEventListener("click", event => {
+// event.preventDefault();
+// if (event.target.id.startsWith("delete")) {
+//     const postId = event.target.id.split("--")[1];
+//     deletePost(postId)
+//     .then(response => {
+//         showPostList();
+//     })
+// }
+// })
+
+// applicationElement.addEventListener("click", event => {
+// event.preventDefault();
+// if (event.target.id.startsWith("edit")) {
+//     const postId = event.target.id.split("--")[1];
+//     getSinglePost(postId)
+//     .then(response => {
+//         showEdit(response);
+//     })
+// }
+// })
 
 // event listener for filtering data based on selection - lesson example
 
@@ -161,75 +238,3 @@ changePostsElement.addEventListener("click", (event) => {
 //     //clear the input fields
 // }
 // })
-
-applicationElement.addEventListener("click", event => {
-event.preventDefault();
-console.log("you clicked submit")
-if (event.target.id === "new-post__submit") {
-    const title = document.querySelector("input[name='postTitle']").value
-    const url = document.querySelector("input[name='postURL']").value
-    const description = document.querySelector("textarea[name='postDescription']").value
-    //we have not created a user yet - for now, we will hard code `1`.
-    const postObject = {
-        title: title,
-        imageURL: url,
-        description: description,
-        userId: 1,
-        timestamp: Date.now()
-    }
-createPost(postObject).then(dbResponse => {
-    showPostList();
-    showPostEntry();
-});
-} else if (event.target.id === "new-post__cancel") {
-    showPostEntry();
-}
-})
-
-// applicationElement.addEventListener("click", event => {
-// event.preventDefault();
-// if (event.target.id.startsWith("delete")) {
-//     const postId = event.target.id.split("--")[1];
-//     deletePost(postId)
-//     .then(response => {
-//         showPostList();
-//     })
-// }
-// })
-
-// applicationElement.addEventListener("click", event => {
-// event.preventDefault();
-// if (event.target.id.startsWith("edit")) {
-//     const postId = event.target.id.split("--")[1];
-//     getSinglePost(postId)
-//     .then(response => {
-//         showEdit(response);
-//     })
-// }
-// })
- 
-applicationElement.addEventListener("click", event => {
-    event.preventDefault();
-    if (event.target.id.startsWith("updatePost")) {
-      const postId = event.target.id.split("__")[1];
-      //collect all the details into an object
-      const title = document.querySelector("input[name='postTitle']").value
-      const url = document.querySelector("input[name='postURL']").value
-      const description = document.querySelector("textarea[name='postDescription']").value
-      const timestamp = document.querySelector("input[name='postTime']").value
-      
-      const postObject = {
-        title: title,
-        imageURL: url,
-        description: description,
-        userId: getLoggedInUser().id,
-        timestamp: parseInt(timestamp),
-        id: parseInt(postId)
-      }
-      
-      updatePost(postObject)
-        .then(response => {
-          showPostList();
-        })
-    }
-  })
